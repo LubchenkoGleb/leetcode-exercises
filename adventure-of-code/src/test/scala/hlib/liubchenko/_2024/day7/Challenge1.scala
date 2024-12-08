4d69603422f35b109f2ea9b8b54b63ad338fce78
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2024-12-08 18:40:22 +0100

    Finish day8 challenge

--- adventure-of-code/src/test/scala/hlib/liubchenko/_2024/day7/Challenge1.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/_2024/day7/Challenge1.scala
@@ -4,17 +4,60 @@ import hlib.liubchenko._2024.Utils
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import scala.annotation.tailrec
+
 class Challenge1 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def calibrate(lines: List[String]): Long = {
+    def validate(expected: Long, numbers: List[Long]): Boolean = {
+      def applyOp(a: Long, b: Long, c: Char) = c match {
+        case '+' => a + b
+        case '*' => a * b
+      }
+
+      @tailrec
+      def calc(numbers: List[Long], operations: List[Char]): Long = numbers match {
+        case a :: b :: Nil => applyOp(a, b, operations.head)
+        case a :: b :: rem => calc(applyOp(a, b, operations.head) +: rem, operations.tail)
+      }
+
+      val n = numbers.length - 1
+      combinations(n, List('+', '*')).exists { calc(numbers, _) == expected }
+    }
+
+    lines
+      .map { str =>
+        str.split(": ") match {
+          case Array(expected, numbers) => expected.toLong -> numbers.split(" ").map(_.toLong).toList
+        }
+      }
+      .collect { case (expected, numbers) if validate(expected, numbers) => expected }
+      .sum
+  }
+
+  def combinations(n: Int, values: List[Char]): List[List[Char]] =
+    if (n == 0) List(Nil)
+    else values.flatMap(v => combinations(n - 1, values).map(rem => v :: rem))
 
   "Day #7 Challenge #1" should {
     "work as expected #1" in {
-      method(List.empty) shouldBe 0
+      calibrate(
+        List(
+          "190: 10 19",
+          "3267: 81 40 27",
+          "83: 17 5",
+          "156: 15 6",
+          "7290: 6 8 6 15",
+          "161011: 16 10 13",
+          "192: 17 8 14",
+          "21037: 9 7 18 13",
+          "292: 11 6 16 20"
+        )
+      ) shouldBe 3749
     }
 
     "work as expected #2" in {
       val input = Utils.readInputFile(7)
-      method(input) shouldBe 0
+      calibrate(input) shouldBe 1399219271639L
     }
   }
 }
--- adventure-of-code/src/test/scala/hlib/liubchenko/_2024/day7/Challenge2.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/_2024/day7/Challenge2.scala
@@ -7,73 +7,58 @@ import org.scalatest.wordspec.AnyWordSpec
 import scala.annotation.tailrec
 
 class Challenge2 extends AnyWordSpec with Matchers {
-  def findRoute(lines: List[String]): Int = {
-    val map = lines.map(_.toArray).toArray
+  def calibrate(lines: List[String]): Long = {
+    def validate(expected: Long, numbers: List[Long]): Boolean = {
+      def applyOp(a: Long, b: Long, c: Char) = c match {
+        case '+' => a + b
+        case '*' => a * b
+        case '|' => (a.toString + b.toString).toLong
+      }
 
-    @tailrec
-    def search(i: Int, j: Int): Option[(Int, Int)] =
-      if (i >= map.length) None
-      else if (j >= map(i).length) search(i + 1, 0)
-      else if (map(i)(j) == '^') Some((i, j))
-      else search(i, j + 1)
+      @tailrec
+      def calc(numbers: List[Long], operations: List[Char]): Long = numbers match {
+        case a :: b :: Nil => applyOp(a, b, operations.head)
+        case a :: b :: rem => calc(applyOp(a, b, operations.head) +: rem, operations.tail)
+      }
 
-    var (i, j) = search(0, 0).get
-    var isExit = false
+      val n = numbers.length - 1
+      combinations(n, List('+', '*', '|')).exists { calc(numbers, _) == expected }
+    }
 
-    def move(nextI: Int, nextJ: Int): Unit = {
-      val direction = map(i)(j)
-
-      if (map(nextI)(nextJ) != '#') {
-        map(i)(j) = 'x'
-        i = nextI; j = nextJ
-        map(i)(j) = direction
-      } else {
-        map(i)(j) = direction match {
-          case '^' => '>'
-          case '>' => 'v'
-          case '<' => '^'
-          case 'v' => '<'
+    lines
+      .map { str =>
+        str.split(": ") match {
+          case Array(expected, numbers) => expected.toLong -> numbers.split(" ").map(_.toLong).toList
         }
       }
-    }
-
-    while (!isExit) {
-      val (nextI, nextJ) = map(i)(j) match {
-        case '^' => (i - 1, j)
-        case '>' => (i, j + 1)
-        case '<' => (i, j - 1)
-        case 'v' => (i + 1, j)
-      }
-      if (nextI < 0 || nextI > map.length - 1 || nextJ < 0 || nextJ > map(0).length - 1) isExit = true
-      else move(nextI, nextJ)
-
-      //println(map.map(_.mkString).mkString("\n") + "\n")
-    }
-
-    map.map(_.count(_ == 'x')).sum + 1
+      .collect { case (expected, numbers) if validate(expected, numbers) => expected }
+      .sum
   }
 
-  "Day #7 Challenge #2" should {
+  def combinations(n: Int, values: List[Char]): List[List[Char]] =
+    if (n == 0) List(Nil)
+    else values.flatMap(v => combinations(n - 1, values).map(rem => v :: rem))
+
+  "Day #7 Challenge #1" should {
     "work as expected #1" in {
-      findRoute(
+      calibrate(
         List(
-          "....#.....",
-          ".........#",
-          "..........",
-          "..#.......",
-          ".......#..",
-          "..........",
-          ".#..^.....",
-          "........#.",
-          "#.........",
-          "......#..."
+          "190: 10 19",
+          "3267: 81 40 27",
+          "83: 17 5",
+          "156: 15 6",
+          "7290: 6 8 6 15",
+          "161011: 16 10 13",
+          "192: 17 8 14",
+          "21037: 9 7 18 13",
+          "292: 11 6 16 20"
         )
-      ) shouldBe 41
+      ) shouldBe 11387
     }
 
     "work as expected #2" in {
-      val input = Utils.readInputFile(6)
-      findRoute(input) shouldBe 0
+      val input = Utils.readInputFile(7)
+      calibrate(input) shouldBe 275791737999003L
     }
   }
 }
