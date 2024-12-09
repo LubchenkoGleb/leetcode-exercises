20e2d250a50daab13d46bf55d9f4542231921853
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2024-12-09 10:31:12 +0100

    Add more templates

---
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day12/Challenge1.scala
@@ -0,0 +1,20 @@
+package hlib.liubchenko.adventofcode._2024.day12
+
+import hlib.liubchenko.adventofcode._2024.Utils
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class Challenge1 extends AnyWordSpec with Matchers {
+  def method(lines: List[String]): Int = 0
+
+  "Day #12 Challenge #1" should {
+    "work as expected #1" in {
+      method(List.empty) shouldBe 0
+    }
+
+    "work as expected #2" in {
+      val input = Utils.readInputFile(12)
+      method(input) shouldBe 0
+    }
+  }
+}
