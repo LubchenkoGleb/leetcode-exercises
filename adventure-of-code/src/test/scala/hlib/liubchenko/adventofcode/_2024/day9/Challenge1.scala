d42b2639c86ee6dc3214005c77f5b82fbecbb927
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2024-12-13 21:51:14 +0100

    Finish day 13

--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day9/Challenge1.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day9/Challenge1.scala
@@ -5,16 +5,51 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Challenge1 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def compactSpace(input: String): Long = {
+    val transformed = input
+      .map(_.asDigit)
+      .sliding(2, 2)
+      .zipWithIndex
+      .flatMap { case (block, i) => List.fill(block.head)(i.toString) ++ block.tail.flatMap(List.fill(_)(".")) }
+      .toArray
+
+    var (start, end) = (0, transformed.length - 1)
+    while (start < end) {
+      if (transformed(start) != ".") start += 1
+      else if (transformed(end) == ".") end -= 1
+      else {
+        transformed(start) = transformed(end)
+        transformed(end) = "."
+        start += 1; end -= 1
+      }
+    }
+
+    transformed.takeWhile(_ != ".").zipWithIndex.map { case (id, i) => id.toLong * i }.sum
+  }
 
   "Day #9 Challenge #1" should {
     "work as expected #1" in {
-      method(List.empty) shouldBe 0
+      // 00...111...2...333.44.5555.6666.777.888899
+
+      // 00...111...2...333.44.5555.6666.777.888899
+      // 009..111...2...333.44.5555.6666.777.88889.
+      // 0099.111...2...333.44.5555.6666.777.8888..
+      // 00998111...2...333.44.5555.6666.777.888...
+      // 009981118..2...333.44.5555.6666.777.88....
+      // 0099811188.2...333.44.5555.6666.777.8.....
+      // 009981118882...333.44.5555.6666.777.......
+      // 0099811188827..333.44.5555.6666.77........
+      // 00998111888277.333.44.5555.6666.7.........
+      // 009981118882777333.44.5555.6666...........
+      // 009981118882777333644.5555.666............
+      // 00998111888277733364465555.66.............
+      // 0099811188827773336446555566..............
+      compactSpace("2333133121414131402") shouldBe 1928
     }
 
     "work as expected #2" in {
-      val input = Utils.readInputFile(9)
-      method(input) shouldBe 0
+      val input = Utils.readInputFileAsString(9)
+      compactSpace(input) shouldBe 6385338159127L
     }
   }
 }
--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day9/Challenge2.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day9/Challenge2.scala
@@ -5,16 +5,64 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Challenge2 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def compactSpace(input: String): Long = {
+    val transformed = input
+      .map(_.asDigit)
+      .sliding(2, 2)
+      .zipWithIndex
+      .flatMap { case (block, i) => List.fill(block.head)(i.toString) ++ block.tail.flatMap(List.fill(_)(".")) }
+      .toArray
+
+    var end = transformed.length - 1
+
+    def findBlockStart: Int = {
+      var i = end
+      while (i > 0 && transformed(i) == transformed(end)) i -= 1
+      i + 1
+    }
+
+    def findEmptySpace(size: Int) = {
+      var (i, space) = (0, 0)
+      while (i < end && space != size) {
+        if (transformed(i) == ".") space += 1
+        else space = 0
+        i += 1
+      }
+      if (space == size) Some(i - size) else None
+    }
+
+    while (end > 1) {
+      if (transformed(end) == ".") end -= 1
+      else {
+        val blockStart = findBlockStart
+        findEmptySpace(end - blockStart + 1).foreach { emptySpaceStart =>
+          (0 to end - blockStart).foreach { i =>
+            transformed(emptySpaceStart + i) = transformed(blockStart + i)
+            transformed(blockStart + i) = "."
+          }
+        }
+
+        end = blockStart - 1
+      }
+
+    }
+
+    transformed.zipWithIndex.collect { case (id, i) if id != "." => id.toLong * i }.sum
+  }
 
   "Day #9 Challenge #2" should {
     "work as expected #1" in {
-      method(List.empty) shouldBe 0
+      // 00...111...2...333.44.5555.6666.777.888899
+      // 0099.111...2...333.44.5555.6666.777.8888..
+      // 0099.1117772...333.44.5555.6666.....8888..
+      // 0099.111777244.333....5555.6666.....8888..
+      // 00992111777.44.333....5555.6666.....8888..
+      compactSpace("2333133121414131402") shouldBe 2858
     }
 
     "work as expected #2" in {
-      val input = Utils.readInputFile(9)
-      method(input) shouldBe 0
+      val input = Utils.readInputFileAsString(9)
+      compactSpace(input) shouldBe 6415163624282L
     }
   }
 }
