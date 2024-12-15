8d0e1e2d009776d5b5bb84c088f2727deddbd1e9
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2024-12-15 18:03:57 +0100

    Finish day 14 task

--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day14/Challenge1.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day14/Challenge1.scala
@@ -5,16 +5,54 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Challenge1 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def countRobots(robots: List[String], width: Int, height: Int, seconds: Int): Long = {
+    val regex = "p=(\\d+),(\\d+) v=([-\\d]+),([-\\d]+)".r
+
+    val (q1, q2, q3, q4) = robots
+      .map { case regex(x, y, vX, vY) => (x.toInt, y.toInt, vX.toInt, vY.toInt) }
+      .map { case (x, y, vX, vY) =>
+        val fX = (x + seconds * vX) % width
+        val fY = (y + seconds * vY) % height
+        (if (fX >= 0) fX else width + fX, if (fY >= 0) fY else height + fY)
+      }
+      .foldLeft((0, 0, 0, 0)) { case ((q1, q2, q3, q4), (x, y)) =>
+        val (wMid, hMid) = (width / 2, height / 2)
+        if (x < wMid && y < hMid) (q1 + 1, q2, q3, q4)
+        else if (x > wMid && y < hMid) (q1, q2 + 1, q3, q4)
+        else if (x < wMid && y > hMid) (q1, q2, q3 + 1, q4)
+        else if (x > wMid && y > hMid) (q1, q2, q3, q4 + 1)
+        else (q1, q2, q3, q4)
+      }
+
+    q1.toLong * q2 * q3 * q4
+  }
 
   "Day #14 Challenge #1" should {
     "work as expected #1" in {
-      method(List.empty) shouldBe 0
+      countRobots(
+        List(
+          "p=0,4 v=3,-3",
+          "p=6,3 v=-1,-3",
+          "p=10,3 v=-1,2",
+          "p=2,0 v=2,-1",
+          "p=0,0 v=1,3",
+          "p=3,0 v=-2,-2",
+          "p=7,6 v=-1,-3",
+          "p=3,0 v=-1,-2",
+          "p=9,3 v=2,3",
+          "p=7,3 v=-1,2",
+          "p=2,4 v=2,-3",
+          "p=9,5 v=-3,-3"
+        ),
+        width = 11,
+        height = 7,
+        seconds = 100
+      ) shouldBe 12
     }
 
     "work as expected #2" in {
       val input = Utils.readInputFile(14)
-      method(input) shouldBe 0
+      countRobots(input, 101, 103, 100) shouldBe 228690000
     }
   }
 }
--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day14/Challenge2.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day14/Challenge2.scala
@@ -5,16 +5,138 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Challenge2 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def countRobots(robots: List[String], width: Int, height: Int, seconds: Int): Int = {
+    val regex = "p=(\\d+),(\\d+) v=([-\\d]+),([-\\d]+)".r
+
+    val originalState = robots
+      .map { case regex(x, y, vX, vY) => (x.toInt, y.toInt, vX.toInt, vY.toInt) }
+
+    (0 to seconds)
+      .find { seconds =>
+        val parsedRobots = originalState
+          .map { case (x, y, vX, vY) =>
+            val fX = (x + seconds * vX) % width
+            val fY = (y + seconds * vY) % height
+            (if (fX >= 0) fX else width + fX, if (fY >= 0) fY else height + fY)
+          }
+
+        val map = Array.fill(height)(Array.fill(width)('.'))
+        parsedRobots.foreach { case (x, y) => map(y)(x) = 'x' }
+        val tree = map.map(_.mkString).mkString("\n")
+        tree.contains("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+      }
+      .getOrElse(seconds)
+  }
 
   "Day #14 Challenge #2" should {
-    "work as expected #1" in {
-      method(List.empty) shouldBe 0
-    }
-
+    //                         x
+    //                              x
+    //                                                                                     x         x
+    //                                              x                                         x
+    //
+    // x             x
+    //
+    //      x     x                                                       x                   x
+    //x                                                                         x
+    //                                          x
+    //
+    //                                                                              x                x
+    //                                x   x               x                              x
+    //                    x
+    //                       x                                                        x
+    //           x           x                     x
+    //                                                   x          x
+    //                  x           x                                                                   x
+    //
+    //                 x                           x
+    //                x  x                                x
+    //
+    //
+    //                            x x     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    // x                                  x                             x
+    //                                    x                             x
+    //   x                                x                             x                          x
+    //                          x         x                             x                   x
+    //                                    x              x              x
+    //                               x    x             xxx             x
+    //                                    x            xxxxx            x
+    //                                    x           xxxxxxx           x                      x        x
+    //                                    x          xxxxxxxxx          x            x                x
+    //                                    x            xxxxx            x        x
+    //                                    x           xxxxxxx           x
+    //                     x            x x          xxxxxxxxx          x x                            x
+    //                                    x         xxxxxxxxxxx         x
+    //                                    x        xxxxxxxxxxxxx        x
+    //                                    x          xxxxxxxxx          x x                  xx
+    //                                    x         xxxxxxxxxxx         x
+    //                                    x        xxxxxxxxxxxxx        x
+    //                      x             x       xxxxxxxxxxxxxxx       x
+    //       x                            x      xxxxxxxxxxxxxxxxx      x
+    //           x  x                     x        xxxxxxxxxxxxx        x                x
+    //                                    x       xxxxxxxxxxxxxxx       x                           x
+    //                                    x      xxxxxxxxxxxxxxxxx      x     x                 x
+    //      x                             x     xxxxxxxxxxxxxxxxxxx     x          x
+    //                x                   x    xxxxxxxxxxxxxxxxxxxxx    x                   x
+    //                                  x x             xxx             x
+    //                                    x             xxx             x      x
+    //                                    x             xxx             x
+    //                                    x                             x
+    //                            x       x                             x
+    //                                    x                             x
+    //               x                    x                             x              x
+    //              x         x           xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                     x
+    //                           x                                        x
+    //                    x     x                x                          x
+    //                            x
+    //                                                                                               x
+    //                         x                                      x
+    //                                                                         x   x
+    //                                            x
+    //                                                                             x        x
+    //
+    //
+    //   x                                            x              x
+    //
+    //
+    //                                                                                    x            x
+    //                                                                  x
+    //                                x                       x
+    //
+    //                      x
+    //                              x            x
+    //                                                              x
+    //                                                                x
+    //                                      x                            x                    x
+    //
+    //
+    //                                                                         x                    x
+    //                                    x       x            x       x                         x
+    //
+    //                                   x                                x
+    //                                                                          x
+    //                                                                     x         x                 x
+    //                                        x           x                   x
+    //                                                                 x    x
+    //                                           x
+    //
+    // x    x                                                                            x
+    //                                                         x
+    //                                                                                          x
+    //                                   x                                                        x
+    //                        x                                      x                          x x
+    //       x                                                                  x                         x
+    //
+    //
+    //                                                                  x                                x
+    //                                                                         x
+    //                          x
+    //                                                                 x
+    //           x
     "work as expected #2" in {
+      // Randomly guessed, not a real solution :(
+
       val input = Utils.readInputFile(14)
-      method(input) shouldBe 0
+      countRobots(input, 101, 103, 100000) shouldBe 7093
     }
   }
 }
