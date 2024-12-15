e77b9f125601dde8b81b1c1d964450f17d08f043
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2024-12-15 23:05:15 +0100

    Day 11 before optimization

--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day11/Challenge1.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day11/Challenge1.scala
@@ -5,16 +5,28 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Challenge1 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def method(stonesStr: String, blinks: Int): Long = {
+    val stones = collection.mutable.ListBuffer.from(stonesStr.split(" ").map(_.toLong))
+
+    def loop(blinks: Int, stone: Long): Long =
+      if (blinks == 0) 1
+      else if (stone == 0) loop(blinks - 1, 1)
+      else if (stone.toString.length % 2 == 0) {
+        val (s1, s2) = stone.toString.splitAt(stone.toString.length / 2)
+        loop(blinks - 1, s1.toLong) + loop(blinks - 1, s2.toLong)
+      } else loop(blinks - 1, stone * 2024)
+
+    stones.map(loop(blinks, _)).sum
+  }
 
   "Day #11 Challenge #1" should {
     "work as expected #1" in {
-      method(List.empty) shouldBe 0
+      method("125 17", 6) shouldBe 22
     }
 
     "work as expected #2" in {
-      val input = Utils.readInputFile(11)
-      method(input) shouldBe 0
+      val input = Utils.readInputFileAsString(11)
+      method(input, 25) shouldBe 220999
     }
   }
 }
--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day11/Challenge2.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day11/Challenge2.scala
@@ -5,16 +5,34 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Challenge2 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def method(stonesStr: String, blinks: Int): Long = {
+    val stones = collection.mutable.ListBuffer.from(stonesStr.split(" ").map(_.toLong))
 
-  "Day #10 Challenge #2" should {
+    val uniqueNumbers = collection.mutable.HashSet.empty[Long]
+
+    def loop(blinks: Int, stone: Long): Long = {
+      uniqueNumbers.add(stone)
+      if (blinks == 0) 1
+      else if (stone == 0) loop(blinks - 1, 1)
+      else if (stone.toString.length % 2 == 0) {
+        val (s1, s2) = stone.toString.splitAt(stone.toString.length / 2)
+        loop(blinks - 1, s1.toLong) + loop(blinks - 1, s2.toLong)
+      } else loop(blinks - 1, stone * 2024)
+    }
+
+    val res = stones.map(loop(blinks, _)).sum
+    println(s"Unique numbers: ${uniqueNumbers.size}")
+    res
+  }
+
+  "Day #11 Challenge #2" should {
     "work as expected #1" in {
-      method(List.empty) shouldBe 0
+      method("125 17", 6) shouldBe 22
     }
 
     "work as expected #2" in {
-      val input = Utils.readInputFile(11)
-      method(input) shouldBe 0
+      val input = Utils.readInputFileAsString(11)
+      method(input, 35) shouldBe 220999
     }
   }
 }
