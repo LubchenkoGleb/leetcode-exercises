f60e8f449dac749dc6e8693167661a70d4816104
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2024-12-15 20:09:12 +0100

    Adventure of code Day 10

--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day10/Challenge1.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day10/Challenge1.scala
@@ -5,16 +5,61 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Challenge1 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def findTrails(lines: List[String]): Int = {
+    val map = lines.map(_.map(_.asDigit)).toArray
+
+    def loop(i: Int, j: Int, prev: Int): List[(Int, Int)] = {
+      if (i < 0 || i >= map.length || j < 0 || j >= map.head.length || map(i)(j) != prev + 1) Nil
+      else if (map(i)(j) == 9) List((i, j))
+      else
+        (loop(i - 1, j, prev + 1) ++
+          loop(i + 1, j, prev + 1) ++
+          loop(i, j - 1, prev + 1) ++
+          loop(i, j + 1, prev + 1)).distinct
+    }
+
+    val routes = for {
+      i <- map.indices
+      j <- map.head.indices
+      if map(i)(j) == 0
+    } yield loop(i, j, -1).length
+
+    routes.sum
+  }
 
   "Day #10 Challenge #1" should {
     "work as expected #1" in {
-      method(List.empty) shouldBe 0
+      findTrails(
+        List(
+          "3330333",
+          "3331333",
+          "3342433",
+          "6543456",
+          "7333337",
+          "8333338",
+          "9333339"
+        )
+      ) shouldBe 2
     }
 
     "work as expected #2" in {
+      findTrails(
+        List(
+          "89010123",
+          "78121874",
+          "87430965",
+          "96549874",
+          "45678903",
+          "32019012",
+          "01329801",
+          "10456732"
+        )
+      ) shouldBe 36
+    }
+
+    "work as expected #3" in {
       val input = Utils.readInputFile(10)
-      method(input) shouldBe 0
+      findTrails(input) shouldBe 611
     }
   }
 }
--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day10/Challenge2.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day10/Challenge2.scala
@@ -5,16 +5,57 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class Challenge2 extends AnyWordSpec with Matchers {
-  def method(lines: List[String]): Int = 0
+  def findTrails(lines: List[String]): Int = {
+    val map = lines.map(_.map(_.asDigit)).toArray
+
+    def loop(i: Int, j: Int, prev: Int): Int = {
+      if (i < 0 || i >= map.length || j < 0 || j >= map.head.length || map(i)(j) != prev + 1) 0
+      else if (map(i)(j) == 9) 1
+      else loop(i - 1, j, prev + 1) + loop(i + 1, j, prev + 1) + loop(i, j - 1, prev + 1) + loop(i, j + 1, prev + 1)
+    }
+
+    val routes = for {
+      i <- map.indices
+      j <- map.head.indices
+      if map(i)(j) == 0
+    } yield loop(i, j, -1)
+
+    routes.sum
+  }
 
   "Day #10 Challenge #2" should {
     "work as expected #1" in {
-      method(List.empty) shouldBe 0
+      findTrails(
+        List(
+          "3330333",
+          "3331333",
+          "3342433",
+          "6543456",
+          "7333337",
+          "8333338",
+          "9333339"
+        )
+      ) shouldBe 2
     }
 
     "work as expected #2" in {
+      findTrails(
+        List(
+          "89010123",
+          "78121874",
+          "87430965",
+          "96549874",
+          "45678903",
+          "32019012",
+          "01329801",
+          "10456732"
+        )
+      ) shouldBe 81
+    }
+
+    "work as expected #3" in {
       val input = Utils.readInputFile(10)
-      method(input) shouldBe 0
+      findTrails(input) shouldBe 1380
     }
   }
 }
