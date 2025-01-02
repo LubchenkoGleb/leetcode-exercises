ed1bb400862b332fe6d36d2478efcf060374528d
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2025-01-02 22:38:25 +0100

    Day 24 challenge 2

--- adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day24/Challenge2.scala
+++ adventure-of-code/src/test/scala/hlib/liubchenko/adventofcode/_2024/day24/Challenge2.scala
@@ -4,121 +4,72 @@ import hlib.liubchenko.adventofcode._2024.Utils
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-import scala.collection.mutable
 import scala.language.implicitConversions
 
 class Challenge2 extends AnyWordSpec with Matchers {
-  def method(lines: List[String], bitsCount: Int, opp: (Long, Long) => Long): String = {
-    val (predefinedWiresLines, rulesLines) = lines.span(_.nonEmpty)
-
-    val initialWires = predefinedWiresLines
-      .map(_.split(": "))
-      .map { case Array(name, value) => name -> toBool(value.toInt) }
-      .toMap
-
-    val ruleRegex = "([a-z0-9]{3}) (XOR|OR|AND) ([a-z0-9]{3}) -> ([a-z0-9]{3})".r
-    val rulesMap = mutable.Map.from(rulesLines.tail.map { case ruleRegex(a, op, b, c) => c -> (a, op, b) }.toMap)
-
-    def check(rulesMap: mutable.Map[String, (String, String, String)]): Int = {
-      val wires = collection.mutable.Map.from(initialWires)
-
-      def loop(cName: String): Boolean = {
-        def calcC = {
-          val (aName, op, bName) = rulesMap(cName)
-          val (a, b) = (loop(aName), loop(bName))
-          op match {
-            case "AND" => a && b
-            case "OR" => a || b
-            case "XOR" => a ^ b
-          }
-        }
-
-        wires.getOrElseUpdate(cName, calcC)
-      }
-
-      rulesMap.keys.foreach(loop)
-
-      val expected = opp(getNumber(wires, "x"), getNumber(wires, "y")).toBinaryString
-      val actual = getNumber(wires, "z").toBinaryString
-      val length = math.max(expected.length, actual.length)
-
-      val expectedF = expected.reverse.padTo(length, ' ').reverse
-      val actualF = actual.reverse.padTo(length, ' ').reverse
-
-      val zipped = expectedF.zip(actualF)
-      val res = zipped.takeWhile { case (a, b) => a == b }
-      res.length
-    }
-
-    var bitsMatches = check(rulesMap)
-    val fixedRules = mutable.Set.empty[String]
-    val iterator = rulesMap.toList.combinations(2)
-
-    while (bitsMatches != bitsCount && iterator.hasNext) {
-      val (aK, aV) :: (bK, bV) :: Nil = iterator.next()
-
-      rulesMap.update(aK, bV)
-      rulesMap.update(bK, aV)
-
-      lazy val checkRes = check(rulesMap)
-      if (fixedRules.intersect(Set(aK, bK)).isEmpty && checkRes > bitsMatches) {
-        fixedRules.add(aK)
-        fixedRules.add(bK)
-        bitsMatches = checkRes
-      } else {
-        rulesMap.update(aK, aV)
-        rulesMap.update(bK, bV)
-      }
-    }
-
-    fixedRules.toList.sorted.mkString(",")
+  implicit class StringOps(key: String) {
+    def isX: Boolean = key.startsWith("x")
+    def isY: Boolean = key.startsWith("y")
+    def isZ: Boolean = key.startsWith("z")
+    def isInput: Boolean = isX || isY
   }
 
-  def getNumber(wires: mutable.Map[String, Boolean], prefix: String): Long = {
-    val res = wires.view
-      .filterKeys(_.startsWith(prefix))
-      .toList
-      .sortBy(_._1)(Ordering[String].reverse)
-      .map(_._2)
-      .map(if (_) 1 else 0)
-      .mkString
+  def initRules(lines: List[String]) = {
+    val (predefinedWiresLines, rulesLines) = lines.span(_.nonEmpty)
 
-    java.lang.Long.parseLong(res, 2)
+    val wires = collection.mutable.Map.empty[String, Boolean]
+    predefinedWiresLines.foreach { str =>
+      val Array(name, value) = str.split(": ")
+      wires.put(name, value.toInt)
+    }
+
+    val ruleRegex = "([a-z0-9]{3}) (XOR|OR|AND) ([a-z0-9]{3}) -> ([a-z0-9]{3})".r
+    rulesLines.tail.map { case ruleRegex(a, op, b, c) =>
+      (a, op, b, c)
+    }
+  }
+
+  def method(lines: List[String]): String = {
+    val rulesMap = initRules(lines)
+
+    // The value of the output is `x XOR y XOR c`, the next carry bit `cN = (x AND y) OR ((x XOR y) AND c)`
+    // but we just care about the fact that:
+    // 1. If the output of a gate is z, then the operation has to be XOR unless it is the last bit.
+    // 2. If the output of a gate is not z and the inputs are not x, y then it has to be AND / OR, but not XOR.
+    // 3. If you have a XOR gate with inputs x, y, there must be another XOR gate with this gate as an input.
+    //    Search through all gates for an XOR-gate with this gate as an input; if it does not exist, your (original) XOR gate is faulty.
+    // 4. if you have an AND-gate, there must be an OR-gate with this gate as an input.
+    //    If that gate doesn't exist, the original AND gate is faulty.
+
+    val wrong1 = rulesMap.filter { case (_, op, _, res) => res.isZ && op != "XOR" && res != "z45" }
+
+    val wrong2 = rulesMap.filter { case (l, op, r, res) => !res.isZ && !l.isInput && !r.isInput && op == "XOR" }
+
+    val wrong3 = rulesMap.filter { case (l, op, r, res) =>
+      (l.isInput || r.isInput) && op == "XOR" &&
+        !rulesMap.exists { case (l2, op2, r2, _) => (l2 == res || r2 == res) && op2 == "XOR" } &&
+        !l.contains("00")
+    }
+    println(wrong3.mkString(", "))
+
+    val wrong4 = rulesMap.filter { case (l, op, r, res) =>
+      ((l.isInput || r.isInput) && op == "AND") &&
+        !rulesMap.exists { case (l2, op2, r2, _) => (l2 == res || r2 == res) && op2 == "OR" } &&
+        !l.contains("00")
+    }
+    println(wrong4.mkString(", "))
+
+    (wrong1 ++ wrong2 ++ wrong3 ++ wrong4).map(_._4).distinct.sorted.mkString(",")
   }
 
   private implicit def toBool(int: Int): Boolean = if (int == 0) false else true
 
-  "Day #24 Challenge #2" should {
-    "work as expected #1" in {
-      val input = List(
-        "x00: 0",
-        "x01: 1",
-        "x02: 0",
-        "x03: 1",
-        "x04: 0",
-        "x05: 1",
-        "y00: 0",
-        "y01: 0",
-        "y02: 1",
-        "y03: 1",
-        "y04: 0",
-        "y05: 1",
-        "",
-        "x00 AND y00 -> z05",
-        "x01 AND y01 -> z02",
-        "x02 AND y02 -> z01",
-        "x03 AND y03 -> z03",
-        "x04 AND y04 -> z04",
-        "x05 AND y05 -> z00"
-      )
-      method(input, 6, _ & _) shouldBe "z00,z01,z02,z05"
-    }
+  "Day #24 Challenge #1" should {
 
     "work as expected #2" in {
-//        List(1,2,3,4).combinations(2).foreach(println)
-
       val input = Utils.readInputFile(24)
-      method(input, 4, _ + _) shouldBe 0
+      method(input) shouldBe "djg,dsd,hjm,mcq,sbg,z12,z19,z37"
     }
+
   }
 }
