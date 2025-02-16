50dc451f47604354f2e6be28bd6f269de6dbb25b
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2025-02-16 22:33:25 +0100

    Optimization improvement attempt

--- top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/backtracking/_6_generate_parentheses.scala
+++ top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/backtracking/_6_generate_parentheses.scala
@@ -4,14 +4,83 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class _6_generate_parentheses extends AnyWordSpec with Matchers {
-  def generateParenthesis(n: Int): List[String] = ???
+  def generateParenthesis(n: Int): List[String] = {
+    def when(cond: => Boolean, value: => List[String]) = if (cond) value else Nil
+
+    def loop(lRem: Int, rRem: Int, acc: String): List[String] =
+      when(lRem == 0 && rRem == 0, List(acc)) ++
+        when(lRem > 0, loop(lRem - 1, rRem, acc + '(')) ++
+        when(lRem < rRem, loop(lRem, rRem - 1, acc + ')'))
+
+    loop(n, n, "")
+  }
 
   "generateParenthesis" should {
-    pending
-
     "work as expected" in {
-      generateParenthesis(3) shouldBe List("((()))", "(()())", "(())()", "()(())", "()()()")
       generateParenthesis(1) shouldBe List("()")
+      generateParenthesis(2) should contain theSameElementsAs List("()()", "(())")
+      generateParenthesis(3) should contain theSameElementsAs List("((()))", "(()())", "(())()", "()(())", "()()()")
+      generateParenthesis(4).sorted should contain theSameElementsAs List(
+        "(((())))",
+        "((()()))",
+        "((())())",
+        "((()))()",
+        "(()(()))",
+        "(()()())",
+        "(()())()",
+        "(())(())",
+        "(())()()",
+        "()((()))",
+        "()(()())",
+        "()(())()",
+        "()()(())",
+        "()()()()"
+      ).sorted
+
+      generateParenthesis(5).sorted should contain theSameElementsAs List(
+        "((((()))))",
+        "(((()())))",
+        "(((())()))",
+        "(((()))())",
+        "(((())))()",
+        "((()(())))",
+        "((()()()))",
+        "((()())())",
+        "((()()))()",
+        "((())(()))",
+        "((())()())",
+        "((())())()",
+        "((()))(())",
+        "((()))()()",
+        "(()((())))",
+        "(()(()()))",
+        "(()(())())",
+        "(()(()))()",
+        "(()()(()))",
+        "(()()()())",
+        "(()()())()",
+        "(()())(())",
+        "(()())()()",
+        "(())((()))",
+        "(())(()())",
+        "(())(())()",
+        "(())()(())",
+        "(())()()()",
+        "()(((())))",
+        "()((()()))",
+        "()((())())",
+        "()((()))()",
+        "()(()(()))",
+        "()(()()())",
+        "()(()())()",
+        "()(())(())",
+        "()(())()()",
+        "()()((()))",
+        "()()(()())",
+        "()()(())()",
+        "()()()(())",
+        "()()()()()"
+      ).sorted
     }
   }
 }
---
+++ top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/hashmap/_9_longest_consecutive_sequence.scala
@@ -0,0 +1,24 @@
+package hlib.liubchenko.topinterview150.hashmap
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class _9_longest_consecutive_sequence extends AnyWordSpec with Matchers {
+  import scala.annotation.tailrec
+
+  def longestConsecutive(nums: Array[Int]): Int = {
+    val set = nums.toSet
+
+    @tailrec
+    def check(element: Int, acc: Int = 1): Int = if (set.contains(element + 1)) check(element + 1, acc + 1) else acc
+
+    set.filterNot(n => set.contains(n - 1)).map(check(_)).maxOption.getOrElse(0)
+  }
+
+  "longestConsecutive" should {
+    "work as expected" in {
+      longestConsecutive(Array(100, 4, 200, 1, 3, 2, 5)) shouldBe 5
+    }
+
+  }
+}
