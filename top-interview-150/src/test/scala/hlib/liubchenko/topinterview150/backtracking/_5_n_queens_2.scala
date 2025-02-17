36722f92e6a033b3bff484dace55b56b32d0da32
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2025-02-17 23:09:07 +0100

    _5_n_queens_2 formatted

--- top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/backtracking/_5_n_queens_2.scala
+++ top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/backtracking/_5_n_queens_2.scala
@@ -7,47 +7,33 @@ class _5_n_queens_2 extends AnyWordSpec with Matchers {
   def totalNQueens(n: Int): Int = {
     val board = Array.fill(n, n)(0)
 
-    def mark(i: Int, j: Int): Seq[(Int, Int)] = {
-      def updateCollect(x: Int, y: Int) =
-        if (x >= 0 && x < n && y >= 0 && y < n && board(x)(y) == 0) { board(x)(y) = 1; List((x, y)) }
-        else Nil
-      val res = (0 until n).flatMap { k =>
-        updateCollect(i, k) ++
-          updateCollect(k, j) ++
-          updateCollect(i + k, j + k) ++
-          updateCollect(i + k, j - k) ++
-          updateCollect(i - k, j - k) ++
-          updateCollect(i - k, j + k)
+    def updateCollect(x: Int, y: Int) =
+      if (x >= 0 && x < n && y >= 0 && y < n && board(x)(y) == 0) {
+        board(x)(y) = 1; List((x, y))
       }
-      board(i)(j) = 2
-      res
+      else Nil
+
+    def mark(i: Int, j: Int): Seq[(Int, Int)] = (0 until n).flatMap { k =>
+      updateCollect(i, k) ++
+        updateCollect(k, j) ++
+        updateCollect(i + k, j + k) ++
+        updateCollect(i + k, j - k) ++
+        updateCollect(i - k, j - k) ++
+        updateCollect(i - k, j + k)
     }
 
     def revert(points: Seq[(Int, Int)]): Unit = points.foreach { case (i, j) => board(i)(j) = 0 }
 
-    def printBoard(msg: String): Unit = println(board.map(_.mkString).mkString(msg + "\n", "\n", "\n"))
+    // @formatter:off
+    def loop(i: Int): Int = if (i == n) 1 else (0 until n).collect { case j if board(i)(j) == 0 =>
+      val affected = mark(i, j)
+      val res = loop(i + 1)
+      revert(affected)
+      res
+    }.sum
+    // @formatter:on
 
-    var iterationsCount = 0
-    def loop(i: Int): Int = {
-      iterationsCount += 1
-      if (i == n) {
-//        printBoard(s"$i")
-        1
-      } else
-        (0 until n).collect {
-          case j if board(i)(j) == 0 =>
-            val affected = mark(i, j)
-            //          printBoard(s"Before: ($i, $j)")
-            val res = loop(i + 1)
-            revert(affected)
-            //          printBoard(s"After: ($i, $j)")
-            res
-        }.sum
-    }
-
-    val res = loop(0)
-    println(iterationsCount)
-    res
+    loop(0)
   }
 
   "totalNQueens" should {
