2c84ea61eb277ee4b9501211e6a7fa088384438a
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2025-03-24 18:37:10 +0100

    Candies WIP

--- top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/array_string/_15_candy.scala
+++ top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/array_string/_15_candy.scala
@@ -5,34 +5,48 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class _15_candy extends AnyWordSpec with Matchers {
   def candy(ratings: Array[Int]): Int = {
+    if (ratings.length == 1) return 1
+
     val candies = Array.fill(ratings.length)(1)
 
-    ratings.zipWithIndex.sliding(3).foreach { case Array((aV, aI), (bV, bI), (cV, cI)) =>
-      if (aV > bV) candies(aI) += 1
-      if (aV < bV) candies(bI) = candies(aI) + 1
-      if (bV > cV) candies(bI) = candies(cI) + 1
-      if (cV > bV) candies(cI) = candies(bI) + 1
-      println(candies.mkString(", "))
+    ratings.zipWithIndex.sliding(2).foreach { case Array((aV, aI), (bV, bI)) =>
+      if (bV > aV) candies(bI) = candies(aI) + 1
     }
-
-    println("before: " + candies.mkString(", "))
-
-    candies.zipWithIndex.sliding(3).foreach { case Array((aV, aI), (bV, bI), (cV, cI)) =>
-      if (aV < bV && bV > cV) candies(bI) = math.max(aV, cV) + 1
+//    println("front: " + candies.mkString(", "))
+    ratings.zipWithIndex.reverse.sliding(2).foreach { case Array((aV, aI), (bV, bI)) =>
+      if (aV < bV) candies(bI) = math.max(candies(bI), candies(aI) + 1)
     }
+//    println("reverse: " + candies.mkString(", "))
 
-    println("after: " + candies.mkString(", "))
+//    ratings.zipWithIndex.sliding(3).foreach { case Array((aV, aI), (bV, bI), (cV, cI)) =>
+////      if (aV > bV) candies(aI) += 1
+////      if (aV < bV) candies(bI) = candies(aI) + 1
+////      if (bV > cV) candies(bI) = candies(cI) + 1
+////      if (cV > bV) candies(cI) = candies(bI) + 1
+//      if (aV <= bV && bV >= cV) candies(bI) = candies(aI) + 1
+//      if (aV > bV && bV < cV) { candies(aI) = math.max(candies(aI), 2); candies(bI) = 1; candies(cI) = 2 }
+//
+//      println(candies.mkString(", "))
+//    }
+
+//    println("before: " + candies.mkString(", "))
+
+//    candies.zipWithIndex.sliding(3).foreach { case Array((aV, aI), (bV, bI), (cV, cI)) =>
+//      if (aV < bV && bV > cV) candies(bI) = math.max(aV, cV) + 1
+//    }
+
+//    println("after: " + candies.mkString(", "))
 
     candies.sum
   }
 
   "candy" should {
     "work as expected" in {
-//      candy(Array(1, 0, 2)) shouldBe 5 // 2, 1, 2
-//      candy(Array(1, 2, 2)) shouldBe 4 // 1, 2, 1
-//      candy(Array(1, 3, 2, 2, 1)) shouldBe 7 // 1, 2, 1, 2, 1
+      candy(Array(1, 0, 2)) shouldBe 5 // 2, 1, 2
+      candy(Array(1, 2, 2)) shouldBe 4 // 1, 2, 1
+      candy(Array(1, 3, 2, 2, 1)) shouldBe 7 // 1, 2, 1, 2, 1
       candy(Array(1, 3, 4, 5, 2)) shouldBe 11 // 1, 2, 3, 4, 1
-//      candy(Array(1, 2, 87, 87, 87, 2, 1)) shouldBe 13 // 1, 2, 3, 1, 3, 2, 1
+      candy(Array(1, 2, 87, 87, 87, 2, 1)) shouldBe 13 // 1, 2, 3, 1, 3, 2, 1
     }
   }
 }
