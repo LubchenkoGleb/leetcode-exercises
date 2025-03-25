38acf81ea645b4483811bf172a5eed60bd563a1a
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2025-03-25 19:26:35 +0100

    top150 productExceptSelf

--- top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/array_string/_13_product_of_array_except_self.scala
+++ top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/array_string/_13_product_of_array_except_self.scala
@@ -4,12 +4,20 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class _13_product_of_array_except_self extends AnyWordSpec with Matchers {
-  def productExceptSelf(nums: Array[Int]): Array[Int] = ???
+  def productExceptSelf(nums: Array[Int]): Array[Int] = {
+    val ans = nums.scan(1)(_ * _).dropRight(1)
+
+    var suffixProduct = 1
+    nums.indices.reverse.foreach { i =>
+      ans(i) *= suffixProduct
+      suffixProduct *= nums(i)
+    }
+
+    ans
+  }
 
   "productExceptSelf" should {
     "work as expected" in {
-      pending
-
       productExceptSelf(Array(1, 2, 3, 4)) shouldBe Array(24, 12, 8, 6)
     }
   }
