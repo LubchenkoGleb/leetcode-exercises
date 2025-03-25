85a5b73429f45348a915fe433696e0f157c6a49c
Hlib Liubchenko <hlib.liubchenko@gmail.com>
2025-03-25 19:35:35 +0100

    top150 longestCommonPrefix

--- top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/array_string/_20_longest_common_prefix.scala
+++ top-interview-150/src/test/scala/hlib/liubchenko/topinterview150/array_string/_20_longest_common_prefix.scala
@@ -4,12 +4,18 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class _20_longest_common_prefix extends AnyWordSpec with Matchers {
-  def longestCommonPrefix(strs: Array[String]): String = ???
+  def longestCommonPrefix(strs: Array[String]): String = {
+    def intersect(a: String, b: String) = {
+      var commonLen = 0
+      while (commonLen < a.length && commonLen < b.length && a(commonLen) == b(commonLen)) commonLen += 1
+      a.take(commonLen)
+    }
+
+    strs.foldLeft(strs.head)(intersect)
+  }
 
   "longestCommonPrefix" should {
     "work as expected" in {
-      pending
-
       longestCommonPrefix(Array("flower", "flow", "flight")) shouldBe "fl"
       longestCommonPrefix(Array("dog", "racecar", "car")) shouldBe ""
     }
